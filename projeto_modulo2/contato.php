ee636d388b364018c2772e416177d6e368e201a4
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-05-24 23:37:06 -0300

    Segundo Commit 24/05
    Criação da estrutura básica do Website, com a página principal e mais algumas. Criei os medias queries para a responsividade nos dispositivos: celulares, tablets e desktops.

---
+++ projeto_modulo2/contato.php
@@ -0,0 +1,8 @@
+<?php include 'includes/header.php'; ?>
+<main>
+    <h1>Contato</h1>
+    <p>E-mail: lieslei.navarro@gmail.com
+        Linkedin: https://www.linkedin.com/in/lieslei-navarro-b84577274/
+    </p>
+</main>
+<?php include 'includes/footer.php'; ?>
