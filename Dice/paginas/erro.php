a56edf20d54d8590c4fe5809b086b6f7fe854909
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-06-17 19:59:26 -0300

    chore
    Atualização na página Sobre a Plataforma, descrição sobre o projeto e games.

--- Dice/paginas/erro.php
+++ Dice/paginas/erro.php
@@ -1,6 +1,4 @@
 <div class="container">
     <p class="center"><img src="imagens/erro.png" alt="Página não encontrada!"></p>
 
-    <h2>A Página que você tentou acessar não foi encontrada!</h2>
-
 </div>
--- Dice/paginas/sobre-a-plataforma.php
+++ Dice/paginas/sobre-a-plataforma.php
@@ -1,5 +1,5 @@
 <div class="container">
 
-    <h2>Sobre a Dice</h2>
+    <h2>A Dice é um Projeto entre colegas que almeja surpreender e conquistar Gamers que são fãs de FPS, MOBA, Estratégia, RPG, RTS, Survival, Plataforma, Sandbox e muitos outros! Temos um grande Catálogo que abranje desde Títulos Originais a games criados por outras Plataformas. </h2>
 
 </div>
