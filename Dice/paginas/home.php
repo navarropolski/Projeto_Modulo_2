338e993cbd7f3527eeda1fb69b50e5864cffe1e1
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-06-17 21:32:35 -0300

    chore
    Adicionei cards exibindo os games disponíveis no catálogo da loja/home.

--- Dice/paginas/home.php
+++ Dice/paginas/home.php
@@ -47,6 +47,65 @@
                 <li><a href="promocoes" title="Promoções">Promoções</a></li>
             </ul>
         </nav>
+            <br>
+            <br>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/eldenring.jpg" class="card-img-top" alt="Elden Ring">
+                        <div class="card-body">
+                            <h5 class="card-title">Elden Ring</h5>
+                            <a href="#" class="btn-comprar">Comprar R$ 199,90</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/cs2.jpg" class="card-img-top" alt="Counter Strike 2">
+                        <div class="card-body">
+                            <h5 class="card-title">Counter Strike 2</h5>
+                            <a href="#" class="btn btn-success">Comprar R$ 99,90</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/dota.jpg" class="card-img-top" alt="Dota 2">
+                        <div class="card-body">
+                            <h5 class="card-title">Dota 2</h5>
+                            <a href="#" class="btn-comprar">Comprar Gratuito</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/dayz.jpg" class="card-img-top" alt="DayZ">
+                        <div class="card-body">
+                            <h5 class="card-title">DayZ</h5>
+                            <a href="#" class="btn-comprar">Comprar R$ 149,90</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/eldenring.jpg" class="card-img-top" alt="Elden Ring">
+                        <div class="card-body">
+                            <h5 class="card-title">Elden Ring</h5>
+                            <a href="#" class="btn-comprar">Comprar R$ 199,90</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="imagens/cs2.jpg" class="card-img-top" alt="Counter Strike 2">
+                        <div class="card-body">
+                            <h5 class="card-title">Counter Strike 2</h5>
+                            <a href="#" class="btn-comprar">Comprar R$ 99,90</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl4OJNyW1kWzoZ6U8CZ4hI4jEonyj1aiuhxr/DJ2Xy"
