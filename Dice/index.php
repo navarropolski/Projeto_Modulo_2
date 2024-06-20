5d500592e7f9c1f8e16328ebec59e74d7778af5f
navarropolski <161781529+navarropolski@users.noreply.github.com>
2024-06-19 22:40:24 -0300

    chore
    Adicionei um z-index para que a rolagem das páginas seja feito da forma correta, sem que os elementos da página sobreponham outros ao sair da tela e também fiz um pequeno ajuste no layout do card dos games.

--- Dice/index.php
+++ Dice/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -6,14 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dice</title>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    
-    <link href="https://fonts.googleapis.com/css2?family=Finlandica:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css2?family=Finlandica:ital,wght@0,400..700;1,400..700&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
@@ -21,8 +22,8 @@
         <a href="home" title="Dice" class="header-logo">
             <img src="imagens/logo.png" alt="Dice">
         </a>
-        
-        <a href="javascript:showMenu()" title="Mostrar menu"  class="header-menu">
+
+        <a href="javascript:showMenu()" title="Mostrar menu" class="header-menu">
             <img src="imagens/menu.webp" alt="Menu">
         </a>
 
@@ -33,42 +34,39 @@
                 <li><a href="cadastre-se" title="Cadastre-se" class="btn-botao">Cadastre-se</a></li>
                 <li><a href="login" title="Login" class="btn-botao">Login</a></li>
             </ul>
-                
+
         </nav>
     </header>
-    
+
     <main>
 
-    <?php
-    
-    if (isset($_GET["param"])) {
-        $param = $_GET["param"];
-        //separar o parâmetro por /
-        $p = explode("/", $param);
+        <?php
+        if (isset($_GET["param"])) {
+            $param = $_GET["param"];
+            $p = explode("/", $param);
+        }
 
-        //print_r($p); verificar se está enviando alguma coisa
-    }
+        $page = $p[0] ?? "home";
+        $jogo = $p[1] ?? NULL;
 
-    $page = $p[0] ?? "home";
-    $jogo = $p[1] ?? NULL;
+        if ($page == "jogo") {
+            $pagina = "jogo/{$jogo}.php";
+        } else {
+            $pagina = "paginas/{$page}.php";
+        }
 
-    if ($page == "jogo") {
-        $pagina = "jogo/{$jogo}.php";
-    } else {
-        $pagina = "paginas/{$page}.php";
-    }
+        if (file_exists($pagina)) {
+            include $pagina;
+        } else {
+            include "paginas/erro.php";
+        }
+        ?>
 
-    //verificar se a página existe
-    if (file_exists($pagina)) {
-        include $pagina;
-    } else {
-        include "paginas/erro.php";
-    }
-    ?>
     </main>
     <footer class="footer">
         <p>Copyright &copy; 2024 - Dice Inc - Todos os direitos reservados.</p>
-        <li><a href="sobre-o-desenvolvedor" title="Sobre o Desenvolvedor" alt="Sobre o Desenvolvedor">Sobre o Desenvolvedor</a></li>
+        <li><a href="sobre-o-desenvolvedor" title="Sobre o Desenvolvedor" alt="Sobre o Desenvolvedor">Sobre o
+                Desenvolvedor</a></li>
     </footer>
 
     <script>
@@ -78,7 +76,9 @@
         }
     </script>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 </body>
 
-</html> 
+</html>
