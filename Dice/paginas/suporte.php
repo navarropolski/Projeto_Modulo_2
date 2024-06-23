a9688cfba3597f304b0be55ddb271bbd16d98b08
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-06-23 02:14:06 -0300

    chore
    Aprimorei o layout da página de Suporte.

--- Dice/paginas/suporte.php
+++ Dice/paginas/suporte.php
@@ -1,7 +1,8 @@
 <div class="container">
     <br><br><br><br>
     <h2>Especifique o problema:</h2>
-    <h2><div class="accordion accordion-flush" id="accordionFlushExample">
+    <h2>
+        <div class="accordion accordion-flush" id="accordionFlushExample">
             <div class="accordion-item">
                 <h2 class="accordion-header">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -12,36 +13,55 @@
                 <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
                         <ul>
-                            <li>Dark Legacy<li>
-                            <li>LabFy Gize</li>    
+                            <li>Dark Legacy</li>
+                            <li>LabFy Gize</li>
                             <li>Snake Game</li>
-                        </ul> 
+                        </ul>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                            Qual problema você está tendo com esse produto?
+                        </button>
+                    </h2>
+                    <div id="flush-collapseTwo" class="accordion-collapse collapse"
+                        data-bs-parent="#accordionFlushExample">
+                        <div class="accordion-body">Placeholder content for this accordion, which is intended to
+                            demonstrate
+                            the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's
+                            imagine
+                            this being filled with some actual content.</div>
+                    </div>
                 </div>
             </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                        Qual problema você está tendo com esse produto? 
-                    </button>
-                </h2>
-                <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate
-                        the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine
-                        this being filled with some actual content.</div>
-                </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Informações adicionais:</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Informações adicionais:</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
 
-        <p class="text-center">
-        <a href="javascript:history.back()" title="Enviar" class="btn-botao">
-            <i class="fas fa-chevron-left"></i>
-            Enviar
-        </a>
-    </p>
+            <p class="text-center">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Enviar requisição
+                </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Recebemos sua requisição e responderemos o mais breve possível, obrigado!
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </p>
     </h2>
 </div>
