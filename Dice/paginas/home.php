c4a40aec00859a177ffd35657859598720c72fd4
Lieslei Adriano Navarro <lieslei.navarro@gmail.com>
2024-06-23 22:01:13 -0300

    chore
    Adicionei no botão comprar e também na imagem dos cards o link para jogar os games.

--- Dice/paginas/home.php
+++ Dice/paginas/home.php
@@ -1,10 +1,12 @@
 <body>
+
+
     <div class="container">
-        
+
         <div class="video-loop-container">
             <video class="video-loop" src="imagens/videoeldenring.webm" autoplay muted loop></video>
-        </div>    
-    
+        </div>
+
         <div id="carouselExampleIndicators" class="carousel slide" style="margin-top: 30px;">
             <div class="carousel-inner">
                 <div class="carousel-item active">
@@ -34,6 +36,7 @@
         <br>
         <br>
         <br>
+
         <nav class="spotlight-nav">
             <ul>
                 <li><a href="categorias" title="Categorias">Categorias</a></li>
@@ -45,39 +48,51 @@
         <br>
 
         <div class="card">
-            <div class="row no-background">
+            <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card mb-4">
-                        <img src="imagens/darklegacy.jpg" class="card-img-top" alt="Dark Legacy">
                         <div class="card-body">
+                            <a href="https://projeto-jogo-blush.vercel.app/index3.html" target="_blank"
+                                title="Dark Legacy">
+                                <img src="imagens/darklegacy.jpg" class="card-img-top" alt="Dark Legacy">
+                            </a>
                             <h5 class="card-title">Dark Legacy</h5>
                             <div class="price-and-button">
                                 <span class="price"></span>
-                                <a href="darklegacy.php" class="btn-botao">Grátis</a>
+                                <a href="https://projeto-jogo-blush.vercel.app/index3.html" target="_blank"
+                                    class="btn-botao">Grátis</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card mb-4">
-                        <img src="imagens/labfygizelogo.jpg" class="card-img-top" alt="LabFy Gize">
                         <div class="card-body">
+                            <a href="https://peaceful-cobbler-6780e6.netlify.app/game" target="_blank"
+                                title="LabFy Gize">
+                                <img src="imagens/labfygizelogo.jpg" class="card-img-top" alt="LabFy Gize">
+                            </a>
                             <h5 class="card-title">LabFy Gize</h5>
                             <div class="price-and-button">
                                 <span class="price"></span>
-                                <a href="https://peaceful-cobbler-6780e6.netlify.app/game" class="btn-botao">Grátis</a>
+                                <a href="https://peaceful-cobbler-6780e6.netlify.app/game" class="btn-botao"
+                                    target="_blank">Grátis</a>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card mb-4">
-                        <img src="imagens/snakelogo.jpeg" class="card-img-top" alt="Snake Game">
                         <div class="card-body">
-                            <h5 class="card-title">Snake Game</h5>
+                            <a href="https://jpaulo-stk.github.io/Snake-Hungry/jogo.html" title="Snake Hungry"
+                                target="_blank">
+                                <img src="imagens/snakelogo.jpeg" class="card-img-top" alt="Snake Hungry">
+                            </a>
+                            <h5 class="card-title">Snake Hungry</h5>
                             <div class="price-and-button">
                                 <span class="price"></span>
-                                <a href="#" class="btn-botao">Grátis</a>
+                                <a href="https://jpaulo-stk.github.io/Snake-Hungry/jogo.html" target="_blank"
+                                    class="btn-botao">Grátis</a>
                             </div>
                         </div>
                     </div>
@@ -86,126 +101,162 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 mb-4">
                     <div class="card mb-4">
-                        <img src="imagens/deeprock.jpg" class="card-img-top" alt="Deep Rock Galactic">
                         <div class="card-body">
+                            <a href="https://store.steampowered.com/app/548430/Deep_Rock_Galactic/"
+                                title="Deep Rock Galactic" target="_blank">
+                                <img src="imagens/deeprock.jpg" class="card-img-top" alt="Deep Rock Galactic">
+                            </a>
                             <h5 class="card-title">Deep Rock Galactic</h5>
                             <div class="price-and-button">
                                 <span class="price">R$ 57,99</span>
-                                <a href="deeprock.php" class="btn-botao">Comprar</a>
+                                <a href="https://store.steampowered.com/app/548430/Deep_Rock_Galactic/" target="_blank"
+                                    class="btn-botao">Comprar</a>
                             </div>
                         </div>
                     </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
+                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/1245620/ELDEN_RING/" title="Elden Ring"
+                            target="_blank">
                             <img src="imagens/eldenring.jpg" class="card-img-top" alt="Elden Ring">
-                            <div class="card-body">
-                                <h5 class="card-title">Elden Ring</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 199,90</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/cs2.jpg" class="card-img-top" alt="Counter Strike 2">
-                            <div class="card-body">
-                                <h5 class="card-title">Counter Strike 2</h5>
-                                <div class="price-and-button">
-                                    <span class="price"></span>
-                                    <a href="#" class="btn-botao">Grátis</a>
-                                </div>
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Elden Ring</h5>
+                            <div class="price-and-button">
+                                <span class="price">R$ 199,90</span>
+                                <a href="https://store.steampowered.com/app/1245620/ELDEN_RING/" target="_blank"
+                                    class="btn-botao">Comprar</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/dota.jpg" class="card-img-top" alt="Dota 2">
-                            <div class="card-body">
-                                <h5 class="card-title">Dota 2</h5>
-                                <div class="price-and-button">
-                                    <span class="price"></span>
-                                    <a href="#" class="btn-botao">Grátis</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/dayz.jpg" class="card-img-top" alt="DayZ">
-                            <div class="card-body">
-                                <h5 class="card-title">DayZ</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 149,90</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/shadowofwar.jpg" class="card-img-top" alt="Shadow of War">
-                            <div class="card-body">
-                                <h5 class="card-title">Shadow of War</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 229,99</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/ageofempires.jpg" class="card-img-top"
-                                alt="Age of Empires II: Definitive Edition">
-                            <div class="card-body">
-                                <h5 class="card-title">Age of Empires II: Definitive Edition</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 36,99</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/hoi4.jpg" class="card-img-top" alt="Hearts of Iron VI">
-                            <div class="card-body">
-                                <h5 class="card-title">Hearts of Iron VI</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 179,99</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-lg-4 col-md-6 mb-4">
-                        <div class="card mb-4">
-                            <img src="imagens/rust.jpg" class="card-img-top" alt="Rust">
-                            <div class="card-body">
-                                <h5 class="card-title">Rust</h5>
-                                <div class="price-and-button">
-                                    <span class="price">R$ 103,49</span>
-                                    <a href="#" class="btn-botao">Comprar</a>
-                                </div>
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/730/CounterStrike_2/" title="Counter Strike 2"
+                        target="_blank">
+                        <img src="imagens/cs2.jpg" class="card-img-top" alt="Counter Strike 2">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Counter Strike 2</h5>
+                            <div class="price-and-button">
+                                <span class="price"></span>
+                                <a href="https://store.steampowered.com/app/730/CounterStrike_2/" target="_blank" 
+                                class="btn-botao">Grátis</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/570/Dota_2/" title="Dota 2"
+                        target="_blank">
+                        <img src="imagens/dota.jpg" class="card-img-top" alt="Dota 2">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Dota 2</h5>
+                            <div class="price-and-button">
+                                <span class="price"></span>
+                                <a href="https://store.steampowered.com/app/570/Dota_2/" target="_blank" 
+                                class="btn-botao">Grátis</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/221100/DayZ/" title="DayZ"
+                        target="_blank">
+                        <img src="imagens/dayz.jpg" class="card-img-top" alt="DayZ">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">DayZ</h5>    
+                            <div class="price-and-button">
+                                <span class="price">R$ 149,90</span>
+                                <a href="https://store.steampowered.com/app/221100/DayZ/" target="_blank"
+                                class="btn-botao">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/356190/Middleearth_Shadow_of_War/" title="Shadow of War"
+                        target="_blank">
+                        <img src="imagens/shadowofwar.jpg" class="card-img-top" alt="Shadow of War">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Shadow of War</h5>
+                            <div class="price-and-button">
+                                <span class="price">R$ 229,99</span>
+                                <a href="https://store.steampowered.com/app/356190/Middleearth_Shadow_of_War/" target="_blank"
+                                    class="btn-botao">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/813780/Age_of_Empires_II_Definitive_Edition/" title="Age of Empires II: Definitive Edition"
+                        target="_blank">
+                        <img src="imagens/ageofempires.jpg" class="card-img-top"
+                            alt="Age of Empires II: Definitive Edition">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Age of Empires II: Definitive Edition</h5>
+                            <div class="price-and-button">
+                                <span class="price">R$ 36,99</span>
+                                <a href="https://store.steampowered.com/app/813780/Age_of_Empires_II_Definitive_Edition/" target="_blank"
+                                class="btn-botao">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/394360/Hearts_of_Iron_IV/" title="Hearts of Iron IV"
+                        target="_blank">
+                        <img src="imagens/hoi4.jpg" class="card-img-top" alt="Hearts of Iron VI">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Hearts of Iron VI</h5>
+                            <div class="price-and-button">
+                                <span class="price">R$ 179,99</span>
+                                <a href="https://store.steampowered.com/app/394360/Hearts_of_Iron_IV/" target="_blank"
+                                    class="btn-botao">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card mb-4">
+                        <a href="https://store.steampowered.com/app/252490/Rust/" title="Rust"
+                        target="_blank">
+                        <img src="imagens/rust.jpg" class="card-img-top" alt="Rust">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Rust</h5>
+                            <div class="price-and-button">
+                                <span class="price">R$ 103,49</span>
+                                <a href="https://store.steampowered.com/app/252490/Rust/" target="_blank"
+                                class="btn-botao">Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 
